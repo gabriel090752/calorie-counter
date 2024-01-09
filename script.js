809c9efd42f02a71ef8dfbec002ab40a6bbf51a7
gabriel090752 <gabrielfdf@ufmg.br>
2024-01-09 18:04:50 -0300

    recipe steps added

--- script.js
+++ script.js
@@ -7,13 +7,18 @@ form.addEventListener("submit",(event) => {
     event.preventDefault()
     const query = textBox.value
     const endPoint = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&query=${query}&number=1`
-
     fetch(endPoint)
         .then(res => res.json())
         .then(data => {
             imgFood.src = data.results[0].image
             document.querySelector('#recipeName').innerHTML = data.results[0].title
+            const id = data.results[0].id
+            fetch(`https://api.spoonacular.com/recipes/${id}/information?apiKey=${apiKey}`)
+                .then(response => response.json())
+                .then(data2 => document.getElementById("food").insertAdjacentHTML('beforeend',data2.instructions))
+                // .then(data2 => console.log(data2.instructions))
         })
+
 
     document.getElementById('searchBarDiv').style.display='none'
     // document.getElementById('searchBarDiv').style.display='block'
