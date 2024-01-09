120fb6ae1c1e21062eaeded93f88c3e29a9d3f97
gabriel090752 <gabrielfdf@ufmg.br>
2024-01-09 01:02:53 -0300

    api request working

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 const textBox = document.getElementById("textBoxId")
 const form = document.getElementById("form")
 const apiKey = "58b5086ad0c24393a8ed607ecb348aef"
-
+let jsonStored
 
 form.addEventListener("submit",(event) => {
     event.preventDefault()
     const query = textBox.value
     const endPoint = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&query=${query}&number=1`
+
     fetch(endPoint)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => {
+            imgFood.src = data.results[0].image
+            document.querySelector('#recipeName').innerHTML = data.results[0].title
+        })
+
+    document.getElementById('searchBarDiv').style.display='none'
+    // document.getElementById('searchBarDiv').style.display='block'
 })
 
 
